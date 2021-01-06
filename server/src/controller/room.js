4baf61ace95d4c7c22ac44feba99e8c290f8702b
Dev Kahar <devkahar99@gmail.com>
2021-01-06 20:04:23 +0530

    Get Room Details

--- server/src/controller/room.js
+++ server/src/controller/room.js
@@ -1,55 +1,82 @@
-const Room = require('../model/rooms');
-const bcrypt = require('bcrypt');
-const { updateOne } = require('../model/user');
-const { addRoomToUser } = require('../util/addRoomToUser');
-const MessageBox = require('../model/messageBox');
+const Room = require("../model/rooms");
+const bcrypt = require("bcrypt");
+const { updateOne, findById } = require("../model/user");
+const { addRoomToUser } = require("../util/addRoomToUser");
+const MessageBox = require("../model/messageBox");
+const User = require("../model/user");
+const user = require("../model/user");
 
-exports.createRoom = async (req,res)=>{
-    const {userId,roomName,roomId,password,participants} = req.body;
-    bcrypt.hash(password,10)
-    .then((_password)=>{
-        const _room = new Room({roomName,roomId,password: _password,participants,creator: userId});
-        _room.save((error,room)=>{
-            if(error) return res.status(400).json({error, message:"Failed TO Create Room"});
-            if (room) {
-                addRoomToUser(userId,room._id);
-                const messageBox = new MessageBox({parentId: room._id}).save();
-                return res.status(201).json({
-                    room,
-                    message: "Room Created Successfully" 
-                })
-            };
-        })
+exports.createRoom = async (req, res) => {
+  const { userId, roomName, roomId, password, participants } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((_password) => {
+      const _room = new Room({
+        roomName,
+        roomId,
+        password: _password,
+        participants,
+        creator: userId,
+      });
+      _room.save((error, room) => {
+        if (error)
+          return res
+            .status(400)
+            .json({ error, message: "Failed TO Create Room" });
+        if (room) {
+          addRoomToUser(userId, room._id);
+          const messageBox = new MessageBox({ parentId: room._id }).save();
+          return res.status(201).json({
+            room,
+            message: "Room Created Successfully",
+          });
+        }
+      });
     })
-    .catch(error => console.log(error));
-}
+    .catch((error) => console.log(error));
+};
 
-exports.addParticipantsInRoom = async(req,res)=>{
-    const {roomId,participants} = req.body;
-    try {
-        Room.findOne({roomId}).exec((error,rooms)=>{
-            if(rooms){
-                
-                const participantsArray = rooms.participants;
-                participants.map(e =>{
-                    addRoomToUser(e.user,rooms._id);
-                    console.log(e.user);
-                    let should = participantsArray.filter(_e => _e.user.toString() === e.user.toString());
-                    console.log("Should",should);
-                    if(should.length === 0){
-                        participantsArray.push(e);
-                    }
-                })
-                Room.updateOne({roomId},{participants: participantsArray}).exec((error,data)=>{
-                    if (data) return res.status(201).json({message:"User Added Success Fully"});
-                    console.log(data);
-                    if(error) return res.status(400).json({error,message:"Failure"});
-                })
-            }
-            if(error) return res.status(400).json({error,message:"Failure"});
-        
-        })
-    }catch (error){
-        console.log(error);
+exports.addParticipantsInRoom = async (req, res) => {
+  const { roomId, participants } = req.body;
+
+  Room.findOne({ roomId }).exec((error, rooms) => {
+    if (rooms) {
+      const participantsArray = rooms.participants;
+      participants.map((e) => {
+        addRoomToUser(e.user, rooms._id);
+        console.log(e.user);
+        let should = participantsArray.filter(
+          (_e) => _e.user.toString() === e.user.toString()
+        );
+        console.log("Should", should);
+        if (should.length === 0) {
+          participantsArray.push(e);
+        }
+      });
+      Room.updateOne({ roomId }, { participants: participantsArray }).exec(
+        (error, data) => {
+          if (data)
+            return res
+              .status(201)
+              .json({ message: "User Added Success Fully" });
+          console.log(data);
+          if (error) return res.status(400).json({ error, message: "Failure" });
+        }
+      );
     }
-}
+    if (error) return res.status(400).json({ error, message: "Failure" });
+  });
+};
+
+exports.getRoomList = (req, res) => {
+    const {roomID} = req.body;
+    Room.findOne({_id: roomID}).populate('creator').exec((error,room)=>{
+        if(error) return res.status(400).json({message:'Invalid RoomId'});
+        if(room){
+            const { roomName,roomId,participants} = room;
+            const {firstName,lastName} = room.creator;
+            console.log(participants,roomName,roomId,firstName,lastName)
+            return res.status(200).json({participants,roomName,roomId,admin:{firstName,lastName}});
+        }
+    })
+};
--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors')
 const env = require('dotenv');
 const {  signIn, userSignup } = require('./controller/user');
-const { createRoom, addParticipantsInRoom } = require('./controller/room');
+const { createRoom, addParticipantsInRoom, getRoomList } = require('./controller/room');
 const { addMessage, getMessage } = require('./controller/message');
 const { isUserAuthentic } = require('./middlewares/authMiddleware');
 env.config();
@@ -14,13 +14,15 @@ app.get('/',(req,res)=>{
     res.send('ok')
 })
 app.post('/api/register',userSignup);
-// app.get('/api/auth',isUserAuthentic);
+//app.get('/api/auth',isUserAuthentic);
 app.post('/api/signin',signIn);
 app.post('/api/room',createRoom);
 app.post('/api/addtoroom',addParticipantsInRoom);
 app.post('/api/message',isUserAuthentic,addMessage);
 app.get('/api/message',isUserAuthentic,getMessage);
-console.log(new Date().toUTCString().split(' '));
+app.get('/api/rooms',isUserAuthentic,getRoomList);
+console.log(new Date().toUTCString(2021, 01, 06, 3, 0, 0).split(' '));
+
 app.listen(2000,()=>{
     console.log('listning on port 2000');
 });
